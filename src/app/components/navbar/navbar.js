723ef9c802158a4daf79f27abf244418ebe2c9b2
Arturs Nazarovs <arturs-nazarovs@inbox.lv>
2023-10-15 18:43:34 +0300

    added navigation and global styles

---
+++ src/app/components/navbar/navbar.js
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from 'next/link'
+
+export default function Navbar() {
+
+    /**
+     * Menu item data
+     */
+    const menuItems = [
+        {
+            name: "Cars",
+            url: "/cars"
+        },
+        {
+            name: "Offers",
+            url: "/offers"
+        }
+    ]
+
+    return (
+        <div className="bg-primary">
+            <div class="container mx-auto flex flex-row">
+                <div className="bg-secondary px-5 py-3">
+                    <span className="text-white text-3xl tracking-wider"><span className="font-extralight">Find</span>Car<strong>24</strong></span>
+                </div>
+                {menuItems.length > 0 && 
+                     <ul>
+                     {menuItems.map((item, id) => (
+                         <li className="p-5 inline-block">
+                             <Link href={item.url} key={id} className="font-bold text-white">
+                                 {item.name}
+                             </Link>
+                         </li>
+                     ))}        
+                 </ul>
+                }
+            </div>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
+    theme: {
+      colors: {
+        white: "#ffffff",
+        lightGray: "#f8f8f8",
+        primary: '#0C2C52',
+        secondary: '#CC00FF'
+      },
+      extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    }
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
